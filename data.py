f04ff255fc9fe410f11ddd24978c50c0b41795a8
jc9361 <jc9361@gmail.com>
2022-10-30 18:56:27 -0700

    splitting up data into seperate file

---
+++ data.py
@@ -0,0 +1,24 @@
+root_msg = f'Welcome to Emerging Technologies API'
+
+info = {'info': 'Welcome to ET Info'};
+
+release_notes = [
+	{'date': '10-31-22', 'msg':'Happy Halloween!', 'link': 'et-dev.firstam.com'},
+	{'date': '10-29-22', 'msg':'Playing with Fast API', 'link': 'et-dev.firstam.com'},
+	{'date': '08-03-22', 'msg':'Weegle Prod v1.0 Release', 'link': 'et-dev.firstam.com'},
+	{'date': '08-03-22', 'msg':'Testing', 'link': 'et-dev.firstam.com'},
+	]
+
+
+projects = [
+	{'date': '00-00-22', 'title':'Project 1', 'description': 'Connecting people and technology.', 'link': 'et-dev.firstam.com'},
+	{'date': '00-00-22', 'title':'Project 2', 'description': 'Connecting people and technology.', 'link': 'et-dev.firstam.com'},
+]
+
+
+products = [
+	{'date': '00-00-22', 'title':'Product 1', 'description': 'Connecting people and technology.', 'link': 'et-dev.firstam.com'},
+	{'date': '00-00-22', 'title':'Product 2', 'description': 'Connecting people and technology.', 'link': 'et-dev.firstam.com'},
+	{'date': '00-00-22', 'title':'Product 3', 'description': 'Connecting people and technology.', 'link': 'et-dev.firstam.com'},
+	{'date': '00-00-22', 'title':'Product 4', 'description': 'Connecting people and technology.', 'link': 'et-dev.firstam.com'},
+]
--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 from fastapi import FastAPI
-
-
-root_msg = f'Welcome to Emerging Technologies'
-
-info = {'info': 'Welcome to ET Info'};
-
-release_notes = [
-        {'date': '10-29-22', 'msg':'Playing with Fast API', 'link': 'et-dev.firstam.com'},
-        {'date': '08-03-22', 'msg':'Weegle Prod v1.0 Release', 'link': 'et-dev.firstam.com'},
-        {'date': '08-03-22', 'msg':'Testing', 'link': 'et-dev.firstam.com'},
-        ]
-
+from data import *
 
 
 #--------------------------
@@ -22,10 +11,19 @@ app = FastAPI()
 async def root():
     return root_msg
 
-@app.get("/info")
+@app.get("/api/info")
 async def root():
     return info
 
-@app.get("/release")
+@app.get("/api/release")
 async def root():
     return release_notes
+
+@app.get("/api/projects")
+async def root():
+    return projects
+
+@app.get("/api/products")
+async def root():
+    return products
+
